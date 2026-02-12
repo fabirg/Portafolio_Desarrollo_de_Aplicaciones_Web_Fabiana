c35d4cbd9b4604752b17714c310f55c5906c0541
fabirg <faroblesgon22@gmail.com>
2026-02-11 22:32:51 -0600

    Commit limpio sin credenciales de Firebase

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -12,36 +12,65 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.io.Serializable;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-@Data
-@Getter
-@Setter
+import java.io.Serializable;
+
+/**
+ *
+ * @author Jose Ortega
+ */
 @Entity
 @Table(name = "categoria")
 public class Categoria implements Serializable {
-
-// Se recomienda añadir un serialVersionUID
     private static final long serialVersionUID = 1L;
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_categoria")
-    public Integer idCategoria;
-
+    private Integer idCategoria;
+    
     @Column(unique = true, nullable = false, length = 50)
     @NotNull
     @Size(max = 50)
     private String descripcion;
-
+    
     @Column(length = 1024)
     @Size(max = 1024)
     private String rutaImagen;
-
+    
     @Column(name = "activo")
     private Boolean activo;
 
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getRutaImagen() {
+        return rutaImagen;
+    }
+
+    public void setRutaImagen(String rutaImagen) {
+        this.rutaImagen = rutaImagen;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
 }
+ 
--- src/main/java/com/tienda/repository/CategoriaRepository.java
+++ src/main/java/com/tienda/repository/CategoriaRepository.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.tienda.repository;
 
@@ -8,8 +8,10 @@ import com.tienda.domain.Categoria;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ *
+ * @author Jose Ortega
+ */
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-
     public List<Categoria> findByActivoTrue();
-
 }
